d730f3ea5dc96f3ad95aee51b5bfd5a6cc248e52
radha kapoor <radha@radhakapoor.net>
2014-11-19 15:14:18 -0500

    refactored jquery

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
 
-$(document).ready(function() {
-	$('.ryu').mouseenter(function() {
-		$('.ryu-still').hide();
-		$('.ryu-ready').show();
-	})
-	.mouseleave(function() {
-		$('.ryu-ready').hide();
-		$('.ryu-still').show();
-	})
-	.mousedown(function() {	
-		playHadouken();		
-		$('.ryu-ready').hide();
-		$('.ryu-throwing').show();		
-		$('.hadouken').finish().show()	
+$(document).ready(function () {
+  $('.ryu').mouseenter(function () {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+    .mouseleave(function () {
+      $('.ryu-ready').hide();
+      $('.ryu-still').show();
+    })
+    .mousedown(function () {
+       playHadouken();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        	
+	  $('.ryu-ready').hide();
+	  $('.ryu-throwing').show();		
+	  $('.hadouken').finish().show()	
 	    .animate(
 		  {'left': '300px'},
 		  500,
@@ -24,10 +24,16 @@ $(document).ready(function() {
 	})
 	
 	.mouseup(function() {
-		$('.ryu-throwing').hide();
-		$('.ryu-ready').show();
-    });    
-})
+	  $('.ryu-throwing').hide();
+	  $('.ryu-ready').show();
+    })    
+});
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume=0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+};
 
 $(document).ready(function() {
 	$('html').on('keydown', function(e) {
@@ -38,20 +44,13 @@ $(document).ready(function() {
 			$('.ryu-cool').show();
 		}
 	})
-})
 
-$(document).ready(function() {
-	$('html').on('keyup', function(e) {
+	.on('keyup', function(e) {
 		var code = e.which;
-		if (code=88){
+		if (code==88){ 
 			$('.ryu-cool').hide();
 			$('.ryu-still').show();
 		}
 	})
-})
+});
 
-function playHadouken () {
-	$('#hadouken-sound')[0].volume=0.5;
-	$('#hadouken-sound')[0].load();
-	$('#hadouken-sound')[0].play();
-};
